9ed4fe84c5136fe7683f0ed69751fc640d0cfcda
Nicolas Macedo de Brito <2401675@salas.aulas>
2025-09-03 09:59:28 -0300

    Atualizado, resta apenas o swagger

--- app.py
+++ app.py
@@ -13,14 +13,14 @@ with app.app_context():
     db.create_all()
 
 # Rotas User
-app.add_url_rule('/', 'index', UserController.index)
-app.add_url_rule('/contact', 'contact', UserController.contact, methods=['GET', 'POST'])
+app.add_url_rule('/users', 'index', UserController.index, methods=['GET'])
+app.add_url_rule('/users', 'contact', UserController.contact, methods=['POST'])
 
 # Rotas Task
 app.add_url_rule('/tasks', 'list_tasks', TaskController.list_tasks, methods=['GET'])
-app.add_url_rule('/tasks/new', 'create_task', TaskController.create_task, methods=['GET', 'POST'])
-app.add_url_rule('/tasks/<int:task_id>/toggle', 'update_task_status', TaskController.update_task_status, methods=['POST'])
-app.add_url_rule('/tasks/delete/<int:task_id>', 'delete_task', TaskController.delete_task, methods=['POST'])
+app.add_url_rule('/tasks', 'create_task', TaskController.create_task, methods=['POST'])
+app.add_url_rule('/tasks/<int:task_id>', 'update_task_status', TaskController.update_task_status, methods=['PUT'])
+app.add_url_rule('/tasks/<int:task_id>', 'delete_task', TaskController.delete_task, methods=['DELETE'])
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5002)
--- controllers/user_controller.py
+++ controllers/user_controller.py
@@ -1,11 +1,12 @@
-from flask import render_template, request, redirect, url_for
+from flask import request, jsonify
 from models.user import db, User
 
 class UserController:
     @staticmethod
     def index():
         users = User.query.all()
-        return render_template('index.html', users=users)
+        users_data = [{'id': user.id, 'name': user.name, 'email': user.email} for user in users]
+        return jsonify(users_data)
     
     @staticmethod
     def contact():
@@ -14,14 +15,13 @@ class UserController:
             email = request.form.get('email')
 
             if not name or not email:
-                return render_template('contact.html', error="Preencha todos os campos")
+                return jsonify({"error": "Preencha todos os campos"}), 400
 
             if User.query.filter_by(email=email).first():
-                return render_template('contact.html', error="Email já cadastrado")
+                return jsonify({"error": "Email já cadastrado"}), 409
 
             new_user = User(name=name, email=email)
             db.session.add(new_user)
             db.session.commit()
-            return redirect(url_for('index'))
-
-        return render_template('contact.html')
+            return jsonify({"message": "Usuário cadastrado com sucesso", "user_id": new_user.id}), 201
+        return jsonify({"error": "Método não permitido"}), 405
